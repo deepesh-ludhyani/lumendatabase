6df0dd5ce1e5cdeda0806edeb263d945521c44c2
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2019-01-28 14:10:40 +0100

    Make search results redact urls for users with no access

--- app/helpers/notices_helper.rb
+++ app/helpers/notices_helper.rb
@@ -79,6 +79,15 @@ module NoticesHelper
     false
   end
 
+  def with_redacted_urls(text)
+    redacted_text = text.gsub(
+      %r{(http[s]?://[w]*[\.]*[^/|$]*)(\S*)},
+      '\1/[REDACTED]'
+    )
+
+    redacted_text
+  end
+
   private
 
   def infringing_url_label(notice)
